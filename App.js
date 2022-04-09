cfe2bf7b1216b8bb9dd660363289923518bf322a
FakharElyes <elyes.fakhar@esprit.tn>
2022-04-09 14:50:06 +0100

    trying out diffrent assests

--- App.js
+++ App.js
@@ -1,30 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, 
-  Text, 
-  View, 
-  SafeAreaView, 
-  Image, 
-  TouchableWithoutFeedback, 
-  TouchableOpacity,
-  TouchableHighlight,
-  Button
-} from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Button
-        title = "Click Me"
-      />
-    </SafeAreaView>
-  );
+  return <ViewImageScreen/> ;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
